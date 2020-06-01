1de3c914f4a8e3acd849eb0e833d57f02816ae07
Aspyn Palatnick <aspyn@seas.upenn.edu>
2020-06-01 13:14:21 -0600

    fix change in session

--- client/src/components/session/Session.js
+++ client/src/components/session/Session.js
@@ -6,9 +6,9 @@ import Video from "../video/Video";
 const Session = (props) => {
   const history = useHistory();
   const isHttps = window.location.protocol.startsWith("https:");
-  const url = `${isHttps ? "wss" : "ws"}://${
-    window.location.hostname
-  }:${8000}/`;
+  const url = `${isHttps ? "wss" : "ws"}://${window.location.hostname}:${
+    window.location.port
+  }/`;
   const socket = new WebSocket(url);
   let sessID = props.sessionID;
 
