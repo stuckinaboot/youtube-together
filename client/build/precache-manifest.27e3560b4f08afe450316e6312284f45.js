42e46da25601059a959c098571d1db82920c1cf9
Aspyn Palatnick <aspyn@seas.upenn.edu>
2020-06-01 13:13:18 -0600

    update version

---
+++ client/build/precache-manifest.27e3560b4f08afe450316e6312284f45.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "784f2d2fdd3c083a4e867e6dcdfd18cd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8185a25caf91427b7e67",
+    "url": "/static/css/main.dd274679.chunk.css"
+  },
+  {
+    "revision": "4c3bf5e2d67c45353494",
+    "url": "/static/js/2.41fa9e0e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.41fa9e0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8185a25caf91427b7e67",
+    "url": "/static/js/main.a7e9e6de.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  }
+]);
