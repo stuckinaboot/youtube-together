feeb1461cb1902b4b7166081b5a062401ae0c761
Aspyn Palatnick <aspyn@seas.upenn.edu>
2020-06-01 22:09:21 -0600

    fix wrong port num

---
+++ client/build/precache-manifest.509b9eae2089b7b8965bdd9fbbad1d0c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2f51f0b51f6b19ae941a31ff428e13b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1698a7d40c74bfa13510",
+    "url": "/static/css/main.dd274679.chunk.css"
+  },
+  {
+    "revision": "3d85afb58e154d4b595d",
+    "url": "/static/js/2.6ea230a3.chunk.js"
+  },
+  {
+    "revision": "a1a636632721a402612df3e043dd630d",
+    "url": "/static/js/2.6ea230a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1698a7d40c74bfa13510",
+    "url": "/static/js/main.dab63f4d.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  }
+]);
